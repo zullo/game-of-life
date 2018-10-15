3e43e3eedd454d7a8bc6e022ada7e443dd70b376
zullo <bartlomiej.grygiel@gmail.com>
2018-10-15 19:00:53 +0200

    really trying

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -9,7 +9,7 @@ package com.wakaleo.gameoflife.domain;
  */
 public enum Cell {
 	// Symbols to represent cell status
-    LIVE_CELL("o"), DEAD_CELL(".");
+    LIVE_CELL("i"), DEAD_CELL(".");
 
     private String symbol;
 
